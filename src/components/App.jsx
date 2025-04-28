191f955420f573746d9f1a1b8d1b9d92de833caa
varshini <varshinisugavanesh@gmail.com>
2025-04-28 22:13:20 +0530

    finishing a quiz

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 // import DateCounter from "./DateCounter";
 
@@ -20,6 +21,7 @@ const App = () => {
     index: 0,
     answer: null,
     points: 0,
+    highscore: 0,
   };
 
   function reducer(state, action) {
@@ -57,14 +59,19 @@ const App = () => {
           index: state.index + 1,
           answer: null,
         };
+      case "finish":
+        return {
+          ...state,
+          status: "finished",
+          highscore:
+            state.points > state.highscore ? state.points : state.highscore,
+        };
       default:
         throw new Error("Action unknown");
     }
   }
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
     (prev, cur) => prev + cur.points,
@@ -101,8 +108,20 @@ const App = () => {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              numQuestions={numQuestions}
+              index={index}
+            />
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+          />
         )}
       </MainSec>
     </div>
--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const NextButton = ({ dispatch, answer }) => {
+const NextButton = ({ dispatch, answer, index, numQuestions }) => {
   if (answer === null) return null;
+  if (index < numQuestions - 1) return;
+  if (index === numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
   return (
     <button
       className="btn btn-ui"
