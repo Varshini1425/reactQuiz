ca9ea0c2a650946b12e4730c3476c72dfb13dbdb
varshini <varshinisugavanesh@gmail.com>
2025-04-28 19:15:39 +0530

    created reducer func

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainSec from "./MainSec";
 import Header from "./Header";
+import { useReducer } from "react";
 // import DateCounter from "./DateCounter";
 
 const App = () => {
+  const initialState = {
+    questions: [],
+    status: "loading",
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "dataReceived":
+        return {
+          ...state,
+          questions: action.payload,
+          status: "ready",
+        };
+      case "dataFailed":
+        return {
+          ...state,
+          status: "failed",
+        };
+      default:
+        throw new Error("Action unknown");
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch(`http://localhost:5174/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       {/* <DateCounter /> */}
