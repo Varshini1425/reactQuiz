5457e6600728dd86b1301654fc38ef79336c5d99
varshini <varshinisugavanesh@gmail.com>
2025-04-28 22:21:39 +0530

    restarting a quiz

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -66,6 +66,20 @@ const App = () => {
           highscore:
             state.points > state.highscore ? state.points : state.highscore,
         };
+      case "restart":
+        return {
+          ...initialState,
+          questions: state.questions,
+          status: "ready",
+        };
+      // return {
+      //   ...state,
+      //   points: 0,
+      //   highscore: 0,
+      //   index: 0,
+      //   answer: null,
+      //   status: "ready",
+      // };
       default:
         throw new Error("Action unknown");
     }
@@ -121,6 +135,7 @@ const App = () => {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </MainSec>
